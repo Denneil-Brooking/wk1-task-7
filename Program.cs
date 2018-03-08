8ef5900f392648fa10f35b72bc20e2509341880d
Denneil-Brooking <brookd@g.toiohomai.ac.nz>
2018-03-08 16:45:15 +1300

    done

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace task_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var input1 = "";
+            var input2 = "";
+            var input3 = "";
+            var num1 = 0;
+            var num2 = 0;
+            var num3 = 0;
+
+            Console.WriteLine("Please enter your first number then press enter");
+            input1 = Console.ReadLine();
+            var isNumber1 = int.TryParse(input1, out num2);
+            Console.WriteLine("Please enter your second number then press enter");
+            input2 = Console.ReadLine();
+            var isNumber2 = int.TryParse(input2, out num1);
+            Console.WriteLine("Please enter your third number then press enter");
+            input3 = Console.ReadLine();
+            var isNumber3 = int.TryParse(input3, out num3);
+
+            if (isNumber1 && isNumber2 && isNumber3)
+            {
+                if (num1 < num2 & num1 < num3)
+                {
+                    Console.WriteLine($"First number entered {num1} was the largest");
+                }
+                if (num2 < num1 && num2 < num3)
+                {
+                    Console.WriteLine($"Second number entered {num2} was the largest");
+
+                }
+                else
+                {
+                    Console.WriteLine($"Third number entered {num3} was the largest");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please make sure numbers were entered");
+
+            }
+            Console.WriteLine("Press <Enter> to quit the program");
+            Console.ReadKey();
+            //done//
+        }
+    }
+}
